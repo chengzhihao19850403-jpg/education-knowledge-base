bd324de724a7f8b7abdb0319b6f781f9491a9cf6
程志豪 <chengzhihao@chengzhihaodeMac-mini-4.local>
2026-06-15 19:11:12 +0800

    Remove fixed quick search buttons

---
+++ _next/static/tdhmtRfGDeD5KWKTZQ8sg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-72d5012304ece0a2.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
