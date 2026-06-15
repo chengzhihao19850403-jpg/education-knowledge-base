9c953f81bc788d51b9c4437c9d6c6d99489a95b6
程志豪 <chengzhihao@chengzhihaodeMac-mini-4.local>
2026-06-15 13:23:33 +0800

    Deploy static site to GitHub Pages

---
+++ _next/static/WawMB6FyvPN669VBmnw9e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d08a851e9a5e5289.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
