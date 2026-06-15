ee992c57f495af23348661f8f2727e92240c8afc
程志豪 <chengzhihao@chengzhihaodeMac-mini-4.local>
2026-06-15 18:11:43 +0800

    Fix GitHub Pages asset paths

---
+++ _next/static/f6aSA9MKRvX0mHbjvp0mz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-c1ec2a2965af3917.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
